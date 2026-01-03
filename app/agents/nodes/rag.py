d2306b176cb3451b9e839268a9b5686623b8e7d9
Vibhu Mishra <vibhumishra@Vibhus-MacBook-Air.local>
2026-01-03 20:02:40 +0530

    remove print statements

--- app/agents/nodes/rag.py
+++ app/agents/nodes/rag.py
@@ -6,10 +6,6 @@ def rag_search_node(state: AgentState):
     db = next(get_db())
     results = resume_search_tool(db, state["query"], top_k=5)
 
-    print("RESULT TYPE:", type(results))
-    print("FIRST RESULT:", results[0])
-    print("FIRST RESULT DIR:", dir(results[0]))
-
     state["resumes"] = results
     state["retrieved_chunks"] = [
         r["resume_text"] for r in results if r.get("resume_text")
